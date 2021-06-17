c9ad30bf4add6c44cdecc931c123eca3f07abd86
ZuchaoWang <zuchao.wang@gmail.com>
2021-06-17 19:10:24 +0800

    Add comment for my own study

--- didact.js
+++ didact.js
@@ -149,11 +149,21 @@ function render(element, container) {
   nextUnitOfWork = wipRoot
 }
 
+// set this to non-null to trigger workLoop for rerender
 let nextUnitOfWork = null
+
+// current finished root; if in the middle of a series of performUnitOfWork, will be previous root
 let currentRoot = null
+
+// current work-in-progress root, in the middle of a series of performUnitOfWork; after all performUnitOfWork will set currentRoot = wipRoot
 let wipRoot = null
+
+// doms to delete from old fiber
 let deletions = null
 
+// runs periodically in the background
+// do prepration works in chunks via a series of performUnitOfWork: create new dom for new fiber, record update/deletion in old fiber
+// but the final commitRoot can not be interrupted, which append all new doms to currentRoot and do the update/deletion
 function workLoop(deadline) {
   let shouldYield = false
   while (nextUnitOfWork && !shouldYield) {
@@ -180,9 +190,12 @@ function performUnitOfWork(fiber) {
   } else {
     updateHostComponent(fiber)
   }
+  // next work on child
   if (fiber.child) {
     return fiber.child
   }
+  // if no child, next work on sibling, or parent's sibling, or parent's parent's sibling
+  // stop when arrive at root
   let nextFiber = fiber
   while (nextFiber) {
     if (nextFiber.sibling) {
@@ -192,6 +205,8 @@ function performUnitOfWork(fiber) {
   }
 }
 
+// used by nearest parent fiber that is function component
+// setup in that function component's performUnitOfWork, won't be changed by children that is not function component
 let wipFiber = null
 let hookIndex = null
 
@@ -199,6 +214,7 @@ function updateFunctionComponent(fiber) {
   wipFiber = fiber
   hookIndex = 0
   wipFiber.hooks = []
+  // execute the function, and the jsx-defined html become its only child
   const children = [fiber.type(fiber.props)]
   reconcileChildren(fiber, children)
 }
@@ -213,13 +229,19 @@ function useState(initial) {
     queue: [],
   }
 
+  // state update applied in next rerender's performUnitOfWork -> updateFunctionComponent -> execute function
   const actions = oldHook ? oldHook.queue : []
   actions.forEach(action => {
     hook.state = action(hook.state)
   })
 
+  // notice here action is a function: oldState => newState
+  // does not support constant
   const setState = action => {
     hook.queue.push(action)
+    // setState will update nextUnitOfWork so workLoop will pick it up to rerender automatically
+    // it seems that multiple action will only lead to one rerender, because workLoop does not run immediately
+    // workLoop only runs when idle, so it's triggered asynchronously
     wipRoot = {
       dom: currentRoot.dom,
       props: currentRoot.props,
@@ -230,18 +252,31 @@ function useState(initial) {
   }
 
   wipFiber.hooks.push(hook)
-  hookIndex++
+  hookIndex++ // hook identified by hookIndex
   return [hook.state, setState]
 }
 
 function updateHostComponent(fiber) {
+  // parent fiber has already identified whether this child fiber is update or create
+  // for update case fiber.dom will be old fiber, so no createDom
+  // only for create case will fiber.dom be null, leading to createDom
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
   }
   reconcileChildren(fiber, fiber.props.children)
 }
 
+// reconciliation called in performUnitOfWork
+// it does not create new dom
+// but it marks child fiber's effectTag as PLACEMENT, and such child fiber will then create dom in its updateHostComponent, if it's not function component
+// it also marks fiber for update/deletion
+// the while loop reach the end of max(elements.length, oldFiber sibling length), missing element or oldFiber will be null
+// this is also the only place to set up fiber.alternate for none-root fiber, actually it's set up by parent fiber for its child fiber
+// fiber.alternate will be oldFiber, and only setup for update case
+// fiber.alternate will be used for child to later get its own old child, or old hook values in performUnitOfWork, or update props in commit phase
 function reconcileChildren(wipFiber, elements) {
+  // this wipFiber argument is not the global wipFiber, it does not change the global wipFiber
+  // should be renamed to avoid confusion
   let index = 0
   let oldFiber =
     wipFiber.alternate && wipFiber.alternate.child
